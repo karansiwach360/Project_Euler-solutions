e3131a849060f0a40c77015c8b7c205231c3bfca
Karan Siwach <karansiwach360@gmail.com>
2017-03-22 19:38:53 +0530

    Create 52.cpp

---
+++ 52.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                  long long
+#define ld                  long double
+#define rep(i,n)            for(ll i=0;i<n;i++)
+#define hell                1000000007LL
+#define vi                  vector<ll>
+#define vii                 vector< vi >
+#define pb                  push_back
+#define mp                  make_pair
+#define fi                  first
+#define se                  second
+#define pii                 pair<ll,ll>
+#define all(c)              c.begin(),c.end()
+#define sz(c)               c.size()
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll power(ll x,ll y,ll p)
+{
+    ll res=1;
+    x=x%p;
+    while(y>0)
+    {
+        if(y&1)
+            res=((res%p)*(x%p))%p;
+        y=y>>1;
+        x=((x%p)*(x%p))%p;
+    }
+    return res;
+}
+bool isprime(ll n)
+{
+    if(n<2) return false;
+    else if(n==2) return true;
+    else if(n%2==0) return false;
+    else
+    {
+        ll z=sqrt(n);
+        rep(i,z-1) if(n%(i+2)==0) return false;
+        return true;
+    }
+}
+vi convert(ll n)
+{
+    vi a;
+    while(n>0)
+    {
+        a.pb(n%10);
+        n/=10;
+    }
+    sort(all(a));
+    return a;
+}
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll N=1000000;
+    for(ll i=1;i<=N;i++)
+    {
+        if(convert(i)==convert(2*i) && convert(2*i)==convert(3*i) && convert(4*i)==convert(5*i) && convert(5*i)==convert(6*i))
+        {
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
